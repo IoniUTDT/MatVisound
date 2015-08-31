fcf4be95a0299aee1572330c1d77b721e848c835
TurinUTDT <ionatan@gmail.com>
2015-08-31 18:56:29 -0300

    Fin del dia

--- SelectSesion.m
+++ SelectSesion.m
@@ -5,9 +5,12 @@ function SelectSesion()
     id=USERS(23).id;
 %% Busca la info del usuario asociado a esa sesion
     
-    User = USERS([USERS.id]==id).userID;
-    IndexLevels = [levelsInstances.sessionId]==id
-    Levels = levelsInstances(IndexLevels);
+    User = USERS([USERS.id]==id).userID; % Busca el id del usuario
+    Levels = levelsInstances([levelsInstances.sessionId]==id); % Selecciona los logs de levels asociados
+    Trials = trialsInstances([trialsInstances.sessionId]==id); % Selecciona los logs de trial asociados
+    
+%% primero graficamos el historial de niveles 
+    
     
 
 end
