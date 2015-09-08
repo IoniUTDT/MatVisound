796e605ba33404334e74deb0c22530e4e9196d38
TurinUTDT <ionatan@gmail.com>
2015-09-08 18:47:52 -0300

    Version actualizada en temas visuales mas datos guardados

--- SelectSesionVisualModificado.m
+++ SelectSesionVisualModificado.m
@@ -23,19 +23,17 @@ disp (['Numero de trials jugados en esta session: ',int2str(length(trials))])
 t_inicial=min([levels.timeStarts]);
 t_final=max([levels.timeExit]);
 
-levelDrawAltura = 6;
+levelDrawAltura = 7;
 levelDrawAlto=1;
 trialDrawAltura = 5;
 trialDrawAlto = 1;
-soundDrawAltura = 4.5;
+soundDrawAltura = 1;
 soundDrawAlto = 0.5;
-touchDrawAltura = 4;
-touchDrawAlto = 0.5;
 
 close all
 figure
 hold on
-axis([(d(t_inicial) - 5/(24*60*3)) (d(t_final) + 5/(24*60*3)) 4 7]);
+axis([(d(t_inicial) - 5/(24*60*3)) (d(t_final) + 5/(24*60*3)) 0 10]);
 title ('Ioni test inicial + test inicial dificil')
 datetick('x','keepticks')
 
@@ -63,9 +61,8 @@ end
 
 %% Graficamos los trials
 
-yt=touchDrawAltura+0.1;
-ys=soundDrawAltura+0.1;
-ytrial=trialDrawAltura+0.1;
+yt=1;
+ys=3;
 for iTrial=1:length(trials)
     trial = trials(iTrial); % Carga el trial a procesar
     % Aca hay un parche para analizar los datos porque no me esta andando
@@ -77,43 +74,16 @@ for iTrial=1:length(trials)
     
     pos=[d(t_entrada) trialDrawAltura (d(t_salida)-d(t_entrada)) trialDrawAlto];
     if (strcmp(trial.tipoDeTrial,'TEST'))
-        % No fijamos si es categoria o si imagen
-        categorias=false;
-        for iCat=1:length(trial.categoriasElementos)
-            if strcmp(trial.categoriasElementos(iCat),'Texto')
-                categorias=true;
-                break
-            end
-        end
-        if categorias
-            color = [1 0.9 0];
-        else
-            color = [1 1 0];
-        end
-        % cambiamos el color si pregunta por paralelismo
-        paralelismo = false;
-        for iCat=1:length(trial.categoriasElementos)
-            if strcmp(trial.categoriasElementos(iCat),'Angulo')
-                paralelismo=true;
-                break
-            end
-        end
-        if paralelismo
-            color = color + [-0.5 -0.5 0.5];
-        end
+        color = 'y';
     elseif (strcmp(trial.tipoDeTrial,'ENTRENAMIENTO'))
         color = 'b';
     end
     rectangle('Position',pos,'FaceColor',color,'EdgeColor',color)
     x = (d(t_salida)+d(t_entrada))/2;
+    y = trialDrawAltura + trialDrawAlto /2;
     texto = [int2str(trial.trialId)];
-    ytrial=ytrial+0.2;
-    if ytrial>trialDrawAltura+trialDrawAlto
-        ytrial=trialDrawAltura+0.1;
-    end
-    text(x,ytrial,texto,'HorizontalAlignment','center');
-    %line([d(t_entrada),d(t_entrada)],[0,10],'Color',color)
-    
+    text(x,y,texto,'HorizontalAlignment','center');
+   
     %% Graficamos los touchs para este trial
     touchs = touchInstances([touchInstances.trialInstance]==trial.trialInstance);
     disp (['Se han encontrado ',int2str(length(touchs)),' toques para procesar en la instancia de trial ',f(trial.trialInstance),' trial id: ',int2str(trial.trialId)])
@@ -131,12 +101,12 @@ for iTrial=1:length(trials)
             color='y';
         end
         
-        line([d(touch.touchInstance),d(touch.touchInstance)],[touchDrawAltura,touchDrawAltura+touchDrawAlto],'Color',color)
+        line([d(touch.touchInstance),d(touch.touchInstance)],[1,2],'Color',color)
         texto = [int2str(touch.idResourceTouched.id)];
         text(d(touch.touchInstance),yt,texto,'HorizontalAlignment','center');
         yt=yt+0.2;
-        if yt>touchDrawAltura+touchDrawAlto
-            yt=touchDrawAltura+0.1;
+        if yt>2
+            yt=1;
         end
     end
     
@@ -152,12 +122,12 @@ for iTrial=1:length(trials)
         %rectangle('Position',pos,'FaceColor',color,'EdgeColor',color)
         %x = (d(sound.stopTime)+d(sound.stopTime))/2;
         
-        line([d(sound.soundInstance),d(sound.soundInstance)],[soundDrawAltura,soundDrawAltura+soundDrawAlto])
+        line([d(sound.soundInstance),d(sound.soundInstance)],[3,4])
         texto = int2str(sound.soundId.id);
         text(d(sound.soundInstance),ys,texto,'HorizontalAlignment','center');
         ys=ys+0.2;
-        if ys>soundDrawAltura+soundDrawAlto
-            ys=soundDrawAltura+0.1;
+        if ys>4
+            ys=3;
         end
     end
 end
